0e4b43c927f1680dfdf2e8bc53c4328e4108fc2c
tetianataker <tetianataker@gmail.com>
2023-10-20 12:21:01 +0200

    set global font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,22 @@
-import { Inter } from "next/font/google";
+import { Jura } from "next/font/google";
 import "./reset.css";
 
 import { APPTITLE } from "../constants";
 
-const inter = Inter({ subsets: ["latin"] });
+const jura = Jura({
+  subsets: ["latin", "latin-ext"],
+  weight: ["300", "400", "500", "600"],
+  display: "swap",
+});
 
 export const metadata = {
   title: { APPTITLE },
-  description: "Page about Tofcio",
+  description: "Page about tofu and Tofcio",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={jura.className}>
       <body>{children}</body>
     </html>
   );
