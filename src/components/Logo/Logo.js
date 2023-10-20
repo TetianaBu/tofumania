1fb6cffcd433aacf10207829a7a420b2dc0174a3
tetianataker <tetianataker@gmail.com>
2023-10-20 12:38:32 +0200

    logo as Image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
 import styles from "./Logo.module.css";
 import logoIcon from "../../../public/logo.svg";
 
 function Logo() {
-  return <div className={styles.logoWrapper}>{logoIcon}</div>;
+  return (
+    <div className={styles.logoWrapper}>
+      <Image priority src={logoIcon} alt="Logo (Image of Tofcio)" />
+    </div>
+  );
 }
 export default Logo;
