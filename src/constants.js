28e12e8ec40253e0838ca70de50dc64c7ebf6bfa
tetianataker <tetianataker@gmail.com>
2023-10-24 12:26:59 +0200

    upgradate data for FIRMNESSLEVELS

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,31 @@
 export const APPTITLE = "TofcioMania";
 
 export const FIRMNESSLEVELS = [
-  "silken",
-  "soft",
-  "medium",
-  "firm",
-  "extra firm",
-  "super firm",
+  {
+    type: "silken",
+    characteristics: "a soft, creamy, and smooth texture",
+    usage: "desserts, smoothies, creame",
+  },
+  {
+    type: "soft",
+    characteristics: "tender and smooth but not as creamy as silken tofu",
+    usage: "scrambles, soups, stews",
+  },
+  {
+    type: "regular",
+    characteristics:
+      "a balanced texture that holds its shape relatively well during cooking",
+    usage: "scrambles, fillings, grilling, baking",
+  },
+  {
+    type: "firm",
+    characteristics: "has a solid, sturdy texture",
+    usage: "stir-frying, grilling, deep-frying",
+  },
+  {
+    type: "extra firm",
+    characteristics:
+      "extremely dense and solid. It holds its shape exceptionally well",
+    usage: "kebabs, skewers, sandwiches, wraps",
+  },
 ];
