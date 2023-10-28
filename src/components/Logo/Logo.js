8e6cf2d652350d50a3f7e7d5c97c8951fdd5a522
tetianataker <tetianataker@gmail.com>
2023-10-28 17:42:05 +0200

    new logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./Logo.module.css";
-import logoIcon from "../../../public/logo.svg";
+import logoIcon from "../../../public/logo-full.svg";
 
 function Logo() {
   return (
     <div className={styles.logoWrapper}>
-      <Image src={logoIcon} alt="Logo (Image of Tofcio)" />
+      <Link href="/">
+        <Image src={logoIcon} alt="Logo (Image of Tofcio)" />
+      </Link>
     </div>
   );
 }
